900c57ef07474559dd3752c2a29df9de55cc10b9
Vishali <22sucg40@tcarts.in>
2024-08-22 23:05:33 +0530

    Update web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -75,6 +75,7 @@ def step_impl(context, element_name):
     element = context.driver.find_element(By.ID, element_id)
     assert(element.get_attribute('value') == u'')
 
+
 ##################################################################
 # These two function simulate copy and paste
 ##################################################################
@@ -95,6 +96,42 @@ def step_impl(context, element_name):
     )
     element.clear()
     element.send_keys(context.clipboard)
+
+## Step Definition for the Button Click##
+
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
+
+## Step Definition for the verifying for a specific name or text to be present##
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    assert(found)
+## Step Definition for the verifying for a specific name or text NOT to be present##
+
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id('search_results')
+    assert(name not in element.text)
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(found)
+
 
 ##################################################################
 # This code works because of the following naming convention:
